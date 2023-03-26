4e016413c51e8593c0e6e56d21959ca7c06ee409
maiuryn <sebastianho0311@gmail.com>
2023-03-25 19:39:19 -0500

    Completed basic game

--- hole.py
+++ hole.py
@@ -1,23 +1,45 @@
 import pygame
 
-hole_size = 20
-flow_speed = 10
-
 class Hole:
-    def __init__(self, x, y):
+    def __init__(self, x, y, difficulty):
         self.x = x
         self.y = y
         self.source_x = x
         self.source_y = y + 15
         self.stream_x = self.source_x
         self.stream_y = self.source_y
+        self.difficulty = difficulty
+        self.leak_rate = 0.2 * self.difficulty
+        self.progress = 0
+        self.clock = pygame.time.Clock()
+        self.hole_size = 20 * self.difficulty
+        self.flow_speed = 10 * self.difficulty
 
     def draw_hole(self, window):
-        pygame.draw.circle(window, (0, 0, 0), (self.x, self.y), hole_size)
+        r = 255 * self.progress if 255 * self.progress <= 255 else 255
+        g = 215 * self.progress if 215 * self.progress <= 215 else 215
+        pygame.draw.circle(window, (r, g, 0, 255), (self.x, self.y), 20)
 
-    def update_leakage(self, window):
-        if self.stream_y < 700:
-            pygame.draw.line(window, (0, 0, 255), (self.source_x, self.source_y), (self.stream_x, self.stream_y + flow_speed), 20)
-            self.stream_y += flow_speed
+    def update_leakage(self, window, screen, water_level):
+        # flow stream
+        if self.stream_y < window.get_height() - water_level:
+            pygame.draw.line(screen, (0, 0, 255, 125), (self.source_x, self.source_y), (self.stream_x, self.stream_y + self.flow_speed), 20)
+            self.stream_y += self.flow_speed
+            return False
+        # stop stream
+        elif self.progress < 1:
+            pygame.draw.line(screen, (0, 0, 255, 125), (self.source_x, self.source_y), (self.stream_x, self.stream_y + self.flow_speed), 20)
+            return True
+        # clear stream
         else:
-            pygame.draw.line(window, (0, 0, 255), (self.source_x, self.source_y), (self.stream_x, self.stream_y), 20)
+            pygame.draw.line(screen, (255, 255, 255, 0), (self.source_x, self.source_y), (self.stream_x, self.stream_y + self.flow_speed), 20)
+            return False
+
+    def is_inside_hole(self, finger_x, finger_y):
+        if (finger_x - self.x) ** 2 + (finger_y - self.y) ** 2 <= self.hole_size ** 2:
+            return True
+        else:
+            return False
+
+    def is_under_water(self, window, water_level):
+        return self.source_y >= window.get_height() - water_level
--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 from hole import Hole
+import time
 
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 pygame.init()
 window = pygame.display.set_mode((1200, 700))
+pygame.display.set_caption("Leaky Window")
+
+surface = pygame.Surface((window.get_width(), window.get_height()), pygame.SRCALPHA)
+
+
 
 clock = pygame.time.Clock()
 
@@ -18,12 +24,25 @@ cap.set(4, 700)
 
 holes = []
 water_level = 0
+difficulty = 1
+start_timer = time.time()
+
+
+def game_over():
+    font = pygame.font.SysFont("calibri", 70, True)
+    text = font.render("Game over!", 1, (255, 0, 0))
+    window.blit(text, (window.get_width() / 2 - text.get_width() / 2, window.get_height() / 2 - text.get_height() / 2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+    pygame.quit()
+    quit()
 
 def add_hole():
-    if len(holes) < 6:
+    while len(holes) < 5:
         x = random.randint(0, window.get_width())
-        y = random.randint(0, window.get_height() - water_level)
-        hole = Hole(x, y)
+        y = random.randint(0, window.get_height() - int(water_level))
+        hole = Hole(x, y, difficulty)
         holes.append(hole)
 
 while True:
@@ -31,25 +50,56 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    ret, img = cap.read()
-    hands, img = detector.findHands(img)
 
+    ret, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     imgRGB = np.rot90(imgRGB)
     frame = pygame.surfarray.make_surface(imgRGB).convert()
+    hands, img = detector.findHands(img)
+
     window.blit(frame, (0, 0))
 
-    for hole in holes:
-        hole.draw_hole(window)
-        hole.update_leakage(window)
+    pygame.draw.rect(surface, (0, 0, 255, 125), (0, window.get_height() - water_level, window.get_width(), water_level))
+    window.blit(surface, (0, 0))
+    total_leak_rate = 0
+
     add_hole()
+    for hole in holes:
+        if hole.progress >= 1:
+            holes.remove(hole)
+
+        if hole.is_under_water(window, water_level):
+            holes.remove(hole)
+            add_hole()
+        if hole.update_leakage(window, surface, water_level):
+            total_leak_rate += hole.leak_rate
+
+        hole.draw_hole(window)
 
     if hands:
         for hand in hands:
             for i in range(4, 21, 4):
-                x = hand["lmList"][i][0]
+                x = window.get_width() - hand["lmList"][i][0] + 60
                 y = hand["lmList"][i][1]
-                # if x, y is within the holes
+                for hole in holes:
+                    if hole.is_inside_hole(x, y):
+                        # hole.progress += hole.clock.tick
+                        hole.progress += 0.1
+                        print(hole.progress)
+
+                    else:
+                        hole.clock.tick()
+
+
+    font = pygame.font.SysFont("calibri", 30, True)
+    timed_score = font.render(f"Tick Tock...{int(time.time() - start_timer)}", 1, (255, 0, 0))
+    window.blit(timed_score, (950, 20))
+
+    water_level += total_leak_rate
+    if water_level >= window.get_height() - 100:
+        game_over()
+        # water_level = 0
+        # holes.clear()
 
     pygame.display.update()
     clock.tick(30)
